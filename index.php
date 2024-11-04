60726a65e7539e5647b5498ac3661b385e327197
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-04 18:09:14 +0700

    add: menambahkan subscribe section

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" class="scroll-smooth">
-
-<head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>News Website</title>
-	<!-- google fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-	<!-- css -->
-	<link rel="stylesheet" href="./assets/css/style.css">
-</head>
-
-<body class="font-inter text-base">
-	<!-- flowbite -->
-	<script src="./node_modules/flowbite/dist/flowbite.min.js"></script>
-	<!-- preline -->
-	<script src="./node_modules/preline/dist/preline.js"></script>
-	<!-- js -->
-	<script src="./assets/js/script.js"></script>
-</body>
-
-</html>
+<?php
+header("Location: ./pages/home.php");
+exit;
