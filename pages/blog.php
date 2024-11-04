33ec22277c6cc214e08858055bd776401bc72a4e
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-04 23:30:45 +0700

    fix: memperbaiki responsive pada file blog.php

--- pages/blog.php
+++ pages/blog.php
@@ -85,7 +85,7 @@
             <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">All News</h2>
             <p class="mt-1 text-gray-600 dark:text-neutral-400">This is a collection of all the news we have.</p>
         </div>
-        <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 items-center justify-center">
+        <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
             <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5" href="#">
                 <div class="aspect-w-16 aspect-h-11">
                     <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image">
@@ -257,63 +257,8 @@
                     </div>
                 </div>
             </a>
-            <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5" href="#">
-                <div class="aspect-w-16 aspect-h-11">
-                    <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image">
-                </div>
-                <div class="my-6">
-                    <h3 class="text-xl font-semibold text-gray-800">
-                        How Google Assistant now helps you record stories for kids
-                    </h3>
-                    <p class="mt-5 text-gray-600">
-                        Google is constantly updating its consumer AI, Google Assistant, with new features.
-                    </p>
-                </div>
-                <div class="mt-auto flex items-center gap-x-3">
-                    <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar">
-                    <div>
-                        <h5 class="text-sm text-gray-800">By Aaron Larsson</h5>
-                    </div>
-                </div>
-            </a>
-            <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5" href="#">
-                <div class="aspect-w-16 aspect-h-11">
-                    <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image">
-                </div>
-                <div class="my-6">
-                    <h3 class="text-xl font-semibold text-gray-800">
-                        How Google Assistant now helps you record stories for kids
-                    </h3>
-                    <p class="mt-5 text-gray-600">
-                        Google is constantly updating its consumer AI, Google Assistant, with new features.
-                    </p>
-                </div>
-                <div class="mt-auto flex items-center gap-x-3">
-                    <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar">
-                    <div>
-                        <h5 class="text-sm text-gray-800">By Aaron Larsson</h5>
-                    </div>
-                </div>
-            </a>
-            <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5" href="#">
-                <div class="aspect-w-16 aspect-h-11">
-                    <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1521321205814-9d673c65c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image">
-                </div>
-                <div class="my-6">
-                    <h3 class="text-xl font-semibold text-gray-800">
-                        Front accounts - let's work together
-                    </h3>
-                    <p class="mt-5 text-gray-600">
-                        Are you an accountant? Are you a company formation advisor?
-                    </p>
-                </div>
-                <div class="mt-auto flex items-center gap-x-3">
-                    <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar">
-                    <div>
-                        <h5 class="text-sm text-gray-800">By Lauren Waller</h5>
-                    </div>
-                </div>
-            </a>
+        </div>
+        <div class="mt-12 text-center">
             <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none mt-6 mx-auto col-span-3">Load more</button>
         </div>
     </section>
