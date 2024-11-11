85cf7e01677427395a260619293732487c861d87
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2024-11-12 00:06:26 +0700

    fix: mengubah icon dashboard pada navbar

--- pages/home.php
+++ pages/home.php
@@ -114,11 +114,11 @@ if ($isLoggedIn) {
                                         <div class="p-1.5 space-y-0.5">
                                             <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                                 href="../admin/dashboard.php">
-                                                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                    <rect width="20" height="14" x="2" y="7" rx="2" ry="2" />
-                                                    <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
+                                                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                    stroke-linecap="round" stroke-linejoin="round">
+                                                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                                                    <polyline points="9 22 9 12 15 12 15 22" />
                                                 </svg>
                                                 Dashboard
                                             </a>
